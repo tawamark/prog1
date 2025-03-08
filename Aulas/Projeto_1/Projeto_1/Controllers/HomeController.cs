f43ea0905bf1ac6be8064175e32e1fd64932d9ae
tawamark <thauangustavok@gmail.com>
2025-03-08 09:50:44 -0300

    Adicionando arquivos do Projeto_1 e Livros

---
+++ Aulas/Projeto_1/Projeto_1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Projeto_1.Models;
+
+namespace Projeto_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Variaveis minhaVariavel = new Variaveis();
+            return View(minhaVariavel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Aulas/Projeto_1/Projeto_1/Models/Variaveis.cs
@@ -0,0 +1,51 @@
+﻿namespace Projeto_1.Models
+{
+    public class Variaveis
+    {
+        // Tipos implícitos
+        // var teste 10;
+
+        // Anotação de Tipos
+        public int userCount = 10;
+
+        // Uma variável pode ser declarada e não inicializada
+        public int totalCount;
+
+        // Constantes
+        // Para declarar uma constante, utilizamos a palavra-chave CONST
+        // No entanto a CONST deve ser inicializada quando declarada
+
+        public const int interestRate = 10;
+
+        public byte minByte = 0;
+        public byte maxByte = 255;
+
+        public sbyte minSbyte = -128;
+        public sbyte maxSbyte = 127;
+
+        public short minShort = -32768;
+        public short maxShort = 32767;
+
+        public ushort minUshort = 0;
+        public ushort maxUshort = 65535;
+
+        public int minInt = -2147483648;
+        public int maxInt = 2147483647;
+
+        public uint minUint = 0;
+        public uint maxUint = 4294967295;
+
+        public long minLong = -9223372036854775808;
+        public long maxLong = 9223372036854775807;
+
+        // O método construtor é invocado na instânciação do objeto por meio da palavra reservada "new". Por regra, o construtor sempre tem o mesmo nome da classe
+        public Variaveis()
+        {
+            totalCount = 0;
+
+            // Tipo implícito
+            // a palavra chave var se encarrega de definir o tipo da variável na instrução de atribuição
+
+        }
+    }
+}
